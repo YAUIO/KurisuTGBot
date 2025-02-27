681831b3d465eb304628e7a42e28e402c6b57269
Yū <artembezzzz@gmail.com>
2025-02-27 13:40:19 +0100

    fixed error for long messages

--- src/main/java/CryptoBot.java
+++ src/main/java/CryptoBot.java
@@ -8,7 +8,9 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
 public class CryptoBot implements LongPollingSingleThreadUpdateConsumer {
     protected TelegramClient telegramClient = new OkHttpTelegramClient(csvParser.readTGKey());
@@ -25,18 +27,29 @@ public class CryptoBot implements LongPollingSingleThreadUpdateConsumer {
                 csvParser.writeChatKey(clients);
             }
 
+            List<String> parsedResponse = new ArrayList<>();
             String response = CommandParser.parse(message_text, listener);
 
+            do {
+                int split = Math.min(response.length(), 4000);
+                split = response.indexOf('\n',split-40);
+                parsedResponse.add(response.substring(0, split));
+                response = response.substring(split);
+            } while (response.length() >= 4000);
+            parsedResponse.add(response);
+
             for (Long chatid : clients) {
-                SendMessage message = SendMessage // Create a message object
-                        .builder()
-                        .chatId(chatid)
-                        .text(response)
-                        .build();
-                try {
-                    telegramClient.execute(message); // Sending our message object to user
-                } catch (TelegramApiException e) {
-                    System.out.println("Exception " + e.getClass() + ": " + e.getMessage());
+                for (String msg : parsedResponse) {
+                    SendMessage message = SendMessage // Create a message object
+                            .builder()
+                            .chatId(chatid)
+                            .text(msg)
+                            .build();
+                    try {
+                        telegramClient.execute(message); // Sending our message object to user
+                    } catch (TelegramApiException e) {
+                        System.out.println("Exception " + e.getClass() + ": " + e.getMessage());
+                    }
                 }
             }
         }
